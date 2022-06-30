0883edc498eba936c1b97a87fa26d31357f938ea
DoretskiyBV <workbd@bk.ru>
2022-06-30 17:21:23 +0300

    Добавил Element+ UI

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,8 +1,15 @@
 <template>
-  <h1>Авторизация</h1>
-    <input type="text" v-model.number="id" placeholder="Введите iD" />
-    <div>Символов: {{inputLength()}}</div>
-    <button>Вход</button>
+  <div class="auth">
+    <h1>LeadHit</h1>
+    <el-form class="auth__form">
+      <el-input type="text"
+                class="auth__input"
+                v-model.number="id"
+                placeholder="id сайта"
+      />
+      <el-button>Вход</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -22,6 +29,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.auth {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  &__form {
+    display: inline-flex;
+    max-width: 500px;
+    background: rgba(255, 68, 2, 0.73);
+    padding: 12px;
+    border-radius: 12px;
+  }
+
+  &__input {
+    width: 400px;
+    margin-right: 12px;
+  }
+}
 </style>
